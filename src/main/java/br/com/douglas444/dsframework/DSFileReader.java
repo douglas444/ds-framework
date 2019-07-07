94adead7bd9569e9cc9f1cd0a8b03c632f750a38
Douglas <douglas.m.cavalcanti@gmail.com>
2019-07-07 15:17:53 -0300

    add option to file that include data and label

--- src/main/java/br/com/douglas444/dsframework/DSFileReader.java
+++ src/main/java/br/com/douglas444/dsframework/DSFileReader.java
@@ -5,17 +5,31 @@ import br.com.douglas444.mltk.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DSFileReader {
 
     private String separator;
     private BufferedReader bufferedReaderData;
     private BufferedReader bufferedReaderLabel;
+    private boolean usingSeparatedFiles;
 
     public DSFileReader(String separator, Reader dataReader, Reader classReader) {
+
+        this.usingSeparatedFiles = true;
         this.separator = separator;
         this.bufferedReaderData = new BufferedReader(dataReader);
         this.bufferedReaderLabel = new BufferedReader(classReader);
+
+    }
+
+    public DSFileReader(String separator, Reader reader) {
+
+        this.usingSeparatedFiles = false;
+        this.separator = separator;
+        this.bufferedReaderData = new BufferedReader(reader);
+
     }
 
     /** Reads the next point of the stream.
@@ -27,21 +41,41 @@ public class DSFileReader {
         String line = bufferedReaderData.readLine();
 
         if (line == null) {
+            bufferedReaderData.close();
+            if (usingSeparatedFiles) {
+                bufferedReaderLabel.close();
+            }
             return null;
         }
 
         String[] splittedLine = line.split(this.separator);
         double[] x = new double[splittedLine.length];
+        int numberOfFeatures = usingSeparatedFiles ? splittedLine.length - 1 : splittedLine.length;
 
-        for (int i = 0; i < splittedLine.length; ++i) {
+        for (int i = 0; i < numberOfFeatures; ++i) {
             x[i] = Double.parseDouble(splittedLine[i]);
         }
 
-        line = bufferedReaderLabel.readLine();
-
-        int y = Integer.parseInt(line);
+        int y;
+        if (usingSeparatedFiles) {
+            line = bufferedReaderLabel.readLine();
+            y = Integer.parseInt(line);
+        } else {
+            y = Integer.parseInt(splittedLine[splittedLine.length - 1]);
+        }
 
         return new Point(x, y);
+    }
+
+
+    public List<Point> next(int n) throws IOException, NumberFormatException {
+
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            points.add(next());
+        }
+        return points;
+
     }
 
 }
