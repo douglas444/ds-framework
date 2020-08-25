ac7b5b66bffc69875c70a7b0859d548b8aa615bc
douglas444 <douglas.m.cavalcanti@gmail.com>
2020-08-25 17:38:08 -0300

    add support for NaN labels

--- src/main/java/br/com/douglas444/dsframework/DSFileReader.java
+++ src/main/java/br/com/douglas444/dsframework/DSFileReader.java
@@ -6,30 +6,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class DSFileReader {
 
     private final String separator;
     private final BufferedReader bufferedReaderData;
     private final BufferedReader bufferedReaderLabel;
-    private final boolean usingSeparatedFiles;
+    private Boolean labelIsNaN;
+    private HashMap<String, Integer> labelEnumeration;
 
     public DSFileReader(String separator, Reader dataReader, Reader labelReader) {
 
-        this.usingSeparatedFiles = true;
         this.separator = separator;
         this.bufferedReaderData = new BufferedReader(dataReader);
         this.bufferedReaderLabel = new BufferedReader(labelReader);
-
+        this.labelIsNaN = null;
     }
 
     public DSFileReader(String separator, Reader reader) {
 
-        this.usingSeparatedFiles = false;
         this.separator = separator;
         this.bufferedReaderData = new BufferedReader(reader);
         this.bufferedReaderLabel = null;
+        this.labelIsNaN = null;
 
     }
 
@@ -40,8 +42,7 @@ public class DSFileReader {
         if (line == null) {
             this.bufferedReaderData.close();
 
-            if (this.usingSeparatedFiles) {
-                assert this.bufferedReaderLabel != null;
+            if (this.bufferedReaderLabel != null) {
                 this.bufferedReaderLabel.close();
             }
 
@@ -49,26 +50,38 @@ public class DSFileReader {
         }
 
         final String[] splittedLine = line.split(this.separator);
-        final int numberOfFeatures = this.usingSeparatedFiles ? splittedLine.length : splittedLine.length - 1;
+        final int numberOfFeatures = this.bufferedReaderLabel != null ? splittedLine.length : splittedLine.length - 1;
         final double[] x = new double[numberOfFeatures];
 
         for (int i = 0; i < numberOfFeatures; ++i) {
             x[i] = Double.parseDouble(splittedLine[i]);
         }
 
-        final int y;
+        final String y;
 
-        if (this.usingSeparatedFiles) {
-
-            assert this.bufferedReaderLabel != null;
+        if (this.bufferedReaderLabel != null) {
             line = this.bufferedReaderLabel.readLine();
-            y = (int) Double.parseDouble(line);
-
+            y = line;
         } else {
-            y = (int) Double.parseDouble(splittedLine[splittedLine.length - 1]);
+            y = splittedLine[splittedLine.length - 1];
         }
 
-        return new Sample(x, y);
+        if (this.labelIsNaN == null) {
+            if (!isNumeric(y)) {
+                this.labelIsNaN = true;
+                this.labelEnumeration = new HashMap<>();
+            } else {
+                this.labelIsNaN = false;
+            }
+        }
+
+        if (this.labelIsNaN) {
+            labelEnumeration.putIfAbsent(y, labelEnumeration.size());
+            return new Sample(x, labelEnumeration.get(y));
+        } else {
+            return new Sample(x, (int) Double.parseDouble(y));
+        }
+
     }
 
 
@@ -79,7 +92,15 @@ public class DSFileReader {
             samples.add(next());
         }
         return samples;
+    }
 
+    private boolean isNumeric(String str) {
+
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+        if (str == null) {
+            return false;
+        }
+        return pattern.matcher(str).matches();
     }
 
 }
